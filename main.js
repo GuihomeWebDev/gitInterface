5b1de47e4699076282c5fba10fcf9e9777eebec9
guihomeWebDev <lebot.guihome@gmail.com>
2022-02-12 09:16:02 +0100

    ⭐ feat(scope):  add controller and update js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 // add ajax useLayoutEffect //  
+const useLayoutEffect = document.getElementById('useLayoutEffect')
+    useEffect(()=>{
+        console.log('useLayoutEffect')
+    })
+
+
+// Language: javascript
+// Path: main.js
+// add ajax useEffect //  
+const useEffect = document.getElementById('useEffect')
+    useEffect(()=>{
+        console.log('useEffect')
+    })
 
   
